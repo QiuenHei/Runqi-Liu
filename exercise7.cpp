510005fd453b169a6045521cae7eb65cacb2784e
刘润錡 <liurunqi@s-MacBook-Air.local>
2019-02-21 16:34:33 -0500

    test
    test

---
+++ exercise7.cpp
@@ -0,0 +1,31 @@
+/* Runqi Liu's exercise 7. 17th Feb 2019. This program write the the implementation
+of the square root function.
+*/
+
+//Include necessary library for input output
+//specify namespace
+#include <iostream>
+#include <string>
+using namespace std;
+
+// successive approximation
+double square(int x)
+{
+  double g = (x*1.0)/2;
+  double xog = (x*1.0)/g;
+  while (!(g==((g+xog)/2)))
+  {
+    g = (g+xog)/2;
+    xog = x/g;
+  }
+  return g;
+}
+//main function
+int main() {
+  int x;
+  cout << "enter a number: " << endl;
+  cin >> x;
+  double result = square(x);
+  cout << "The square root is: "<< result << endl;
+  return 0;
+}
